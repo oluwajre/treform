c6c2ba8b4b2bdb7ae162d3f0f2bd23028b708e69
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2024-11-15 23:42:53 +0100

    Second Commit

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Banner.css';
-import { polygon_1, polygon_2, speaker_picture, red_stamp, trenova_logo } from '../../Constants/banner';
+import { polygon_1, polygon_2, speaker_picture, red_stamp, trenova_logo, napps_logo } from '../../Constants/banner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays} from '@fortawesome/free-regular-svg-icons';
 
@@ -10,8 +10,17 @@ const Banner = () => {
     <section className="banner" id="banner">
         <img src={polygon_2} alt="background polygon 1" className='z-1 background-shape2'/>
         <img src={polygon_1} alt="background polygon 1" className='z-1 background-shape1'/>
-        <img src={trenova_logo} alt="trenova logo" className='z-2 main-logo position-absolute'/>
-        <div className="z-2 container my-5 position-relative">
+
+        <div className="container mt-1">
+        <div className="logo-container d-flex justify-content-end align-items-center">
+          <img src={napps_logo} alt="trenova logo" className='z-2 logo mx-2'/>
+          <p className="text-dark text-uppercase m-0 mx-2">in conjuction with</p>
+          <img src={trenova_logo} alt="trenova logo" className='z-2 logo'/>
+        </div>
+          </div> 
+        
+        
+        <div className="z-2 container mb-5 mt-2 position-relative">
           <div className="row align-items-end">
             <div className="col-8 col-sm-6 position-relative">
               <h1 className='display-3 lh'> Scale <span className='text-primary'>up</span> Series <span className='d-none d-md-inline-block'>Webinar</span> <span className='text-primary'>3.7</span></h1>
@@ -38,7 +47,13 @@ const Banner = () => {
           
         </div>
 
-        <div className="backround-circle position-absolute top-100 start-100 translate-middle"></div>
+        <div className="backround-circle position-absolute top-100 start-100 translate-middle">
+          <p className="text-white position-absolute translate-middle speaker-text text-center">
+            <span className='fw-bold'>Lead Speaker:</span>
+            <span>Ajayi Samuel</span>
+            <span>(C.E.O TreNova Group)</span>
+          </p>
+        </div>
     </section>
   )
 }
--- src/Constants/banner.js
+++ src/Constants/banner.js
@@ -3,6 +3,8 @@ import polygon_2 from '../assets/images/banner/polygon-2.svg';
 import red_stamp from '../assets/images/banner/red-stamp.svg';
 import trenova_logo from '../assets/images/banner/trenova-logo.png';
 import speaker_picture from '../assets/images/banner/speaker-picture.png';
+import napps_logo from '../assets/images/banner/napps-logo.png';
 
-export { polygon_1, polygon_2, red_stamp, trenova_logo, speaker_picture };
+
+export { polygon_1, polygon_2, red_stamp, trenova_logo, speaker_picture, napps_logo };
 
